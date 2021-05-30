662db2100ce6731873403334872c21dc380f9e9d
Dan Martin <66933628+DSM9606@users.noreply.github.com>
2021-05-30 00:52:10 -0400

    Validated Users

--- game.py
+++ game.py
@@ -6,3 +6,14 @@ user_choice = input("Please choose one of 'rock', 'paper', 'scissors': ")
 
 print("User Choice: ", user_choice)
 
+if (user_choice == "rock") or (user_choice == "paper") or (user_choice == "scissors"):
+    print("Valid. KEEP GOING")
+else:
+     print("INVALID PLAY AGAIN") 
+     exit()
+
+
+
+
+
+print("THIS IS THE END OF OUR GAME. PLEASE PLAY AGAIN")
