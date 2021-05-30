62ce21274fe29e91a810751970fadb175f43588f
Dan Martin <66933628+DSM9606@users.noreply.github.com>
2021-05-30 17:13:17 -0400

    Update game.py

--- game.py
+++ game.py
@@ -16,7 +16,7 @@ print(" ")
 print(" ")
 print(" ")
 
-print(PLAYER_NAME, " WELCOME TO Rock, Paper, Scissors, Shoot!")
+print("Player ", PLAYER_NAME," WELCOME TO Rock, Paper, Scissors, Shoot!")
 
 print(" ")
 print(" ")
@@ -30,7 +30,7 @@ print(" ")
 print(" ")
 print(" ")
 
-print("User Choice: ", user_choice)
+print(PLAYER_NAME, "'s Choice: ", user_choice)
 
 print(" ")
 print(" ")
@@ -38,9 +38,9 @@ print(" ")
 print(" ")
 
 if (user_choice == "rock") or (user_choice == "paper") or (user_choice == "scissors"):
-    print("It looks like your choice,",user_choice,", is a valid choice!! SO LETS KEEP ON GOING!!")
+    print(PLAYER_NAME, "It looks like your choice,",user_choice,", is a valid choice!! SO LETS KEEP ON GOING!!")
 else:
-     print("INVALID CHOICE PLEASE PLAY AGAIN AND REMEMBER THE INPUTS ARE CASE SENSITIVE!!") 
+     print(PLAYER_NAME, "INVALID CHOICE PLEASE PLAY AGAIN AND REMEMBER THE INPUTS ARE CASE SENSITIVE!!") 
      exit()
 
 print(" ")
@@ -64,26 +64,26 @@ print(" ")
 print(" ")
 
 if computer_choice == user_choice:
-    print("It's a Tie!!, What a Coincidence!!")
+    print(PLAYER_NAME, "It's a Tie!!, What a Coincidence!!")
 elif computer_choice == "rock" and user_choice == "paper":
-    print("Congratulations You Won!! , Paper Covers Rock!! ")
+    print(PLAYER_NAME, "Congratulations You Won!! , Paper Covers Rock!! ")
 elif computer_choice == "rock" and user_choice == "scissors":
-    print("OH NO THE COMPUTER WON THIS ROUND!! - Rock Crushes Scissors!!")
+    print(PLAYER_NAME, "OH NO THE COMPUTER WON THIS ROUND!! - Rock Crushes Scissors!!")
 elif computer_choice == "paper" and user_choice == "rock":
-    print("OH NO THE COMPUTER WON THIS ROUND!! -  Paper covers Rock")
+    print(PLAYER_NAME, "OH NO THE COMPUTER WON THIS ROUND!! -  Paper covers Rock")
 elif computer_choice == "paper" and user_choice == "scissors":
-    print("Congratulations You Won!! , Scissors cut Paper!! ")
+    print(PLAYER_NAME, "Congratulations You Won!! , Scissors cut Paper!! ")
 elif computer_choice == "scissors" and user_choice == "paper":
-    print("OH NO THE COMPUTER WON THIS ROUND!! - Scissors cut Paper!! " )
+    print(PLAYER_NAME, "OH NO THE COMPUTER WON THIS ROUND!! - Scissors cut Paper!! " )
 elif computer_choice == "scissors" and user_choice == "rock":
-    print("Congratulations You Won!! , Rock Crushes Scissors!! ")
+    print(PLAYER_NAME, "Congratulations You Won!! , Rock Crushes Scissors!! ")
 
 print(" ")
 print(" ")
 print(" ")
 print(" ")
 
-print("THIS IS THE END OF OUR GAME!! THANK YOU FOR PLAYING, PLEASE PLAY AGAIN!!")
+print(PLAYER_NAME, "THIS IS THE END OF OUR GAME!! THANK YOU FOR PLAYING, PLEASE PLAY AGAIN!!")
 
 print(" ")
 print(" ")
